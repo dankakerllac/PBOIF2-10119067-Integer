4474bd8dc4d9a6352683306614fbd65bd764d70d
User <User@DESKTOP-6N6EHH6>
2020-10-19 22:28:16 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan7/integer/PBOIF210119067Latihan7Integer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan2.deklarasivariabel;
+
+/**
+ *
+ *
+ * NAMA     : Andreas Danhago
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi Program : Deklarasi & Pengaksesan
+ */
+public class PBOIF210119067Latihan1Biodata {
+/**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
